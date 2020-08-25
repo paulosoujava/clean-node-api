cf2c877f487debe0df68101b79006c7ef246b269
Paulo jorge <paulosoujava@gmail.com>
2020-08-25 06:30:40 -0300

    chore: update librarys

---
+++ src/presentation/controller/signup.spec.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const htttpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httResponse = sut.handle(htttpRequest)
+    expect(httResponse.statusCode).toBe(400)
+  })
+})
