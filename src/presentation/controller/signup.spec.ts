ea031f7cdc73e3a54c4d876dbf86163eab405ed4
Paulo jorge <paulosoujava@gmail.com>
2020-08-25 06:51:14 -0300

    feat: section 2  class 11

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -12,5 +12,19 @@ describe('SignUp Controller', () => {
     }
     const httResponse = sut.handle(htttpRequest)
     expect(httResponse.statusCode).toBe(400)
+    expect(httResponse.body).toEqual(new Error('Missing param: name'))
+  })
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const htttpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httResponse = sut.handle(htttpRequest)
+    expect(httResponse.statusCode).toBe(400)
+    expect(httResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
