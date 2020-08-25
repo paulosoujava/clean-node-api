9288e5b6892c13d047c15182491705231daf85cc
Paulo jorge <paulosoujava@gmail.com>
2020-08-25 07:15:11 -0300

    feat: section 3 class 12

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -1,4 +1,5 @@
 import { SignUpController } from './signup'
+import { MissingParamError } from '../errors/missing-param-error'
 
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
@@ -12,7 +13,7 @@ describe('SignUp Controller', () => {
     }
     const httResponse = sut.handle(htttpRequest)
     expect(httResponse.statusCode).toBe(400)
-    expect(httResponse.body).toEqual(new Error('Missing param: name'))
+    expect(httResponse.body).toEqual(new MissingParamError('name'))
   })
   test('Should return 400 if no email is provided', () => {
     const sut = new SignUpController()
@@ -25,6 +26,32 @@ describe('SignUp Controller', () => {
     }
     const httResponse = sut.handle(htttpRequest)
     expect(httResponse.statusCode).toBe(400)
-    expect(httResponse.body).toEqual(new Error('Missing param: email'))
+    expect(httResponse.body).toEqual(new MissingParamError('email'))
+  })
+  test('Should return 400 if no password is provided', () => {
+    const sut = new SignUpController()
+    const htttpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httResponse = sut.handle(htttpRequest)
+    expect(httResponse.statusCode).toBe(400)
+    expect(httResponse.body).toEqual(new MissingParamError('password'))
+  })
+  test('Should return 400 if no passwordConfirmation is provided', () => {
+    const sut = new SignUpController()
+    const htttpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password'
+      }
+    }
+    const httResponse = sut.handle(htttpRequest)
+    expect(httResponse.statusCode).toBe(400)
+    expect(httResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
   })
 })
--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,15 +1,14 @@
-export class SignUpController {
-  handle (httpRequest: any): any {
-    if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new Error('Missing param: name')
-      }
-    }
-    if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new Error('Missing param: email')
+import { HttpRequest, HttpResponse } from '../protocols/http'
+import { badRequest } from '../helpers/http-helper'
+import { MissingParamError } from '../errors/missing-param-error'
+import { Controller } from '../protocols/controller'
+
+export class SignUpController implements Controller {
+  handle (httpRequest: HttpRequest): HttpResponse {
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
       }
     }
   }
