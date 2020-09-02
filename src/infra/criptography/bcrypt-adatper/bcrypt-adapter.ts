43889c5d791f4fad575fa9290cfcc730818a7f73
Paulo jorge <paulosoujava@gmail.com>
2020-09-02 14:47:17 -0300

    feat: section 13 class 42

---
+++ src/infra/criptography/bcrypt-adatper/bcrypt-adapter.ts
@@ -0,0 +1,20 @@
+import bcrypt from 'bcrypt'
+import { Hasher } from '../../../data/protocols/criptography/hasher'
+import { HasheComparer } from '../../../data/protocols/criptography/hash-comparer'
+
+export class BcrypterAdapter implements Hasher, HasheComparer {
+  private readonly salt: number
+  constructor (salt: number) {
+    this.salt = salt
+  }
+
+  async compare (value: string, hashe: string): Promise<boolean> {
+    const isValid = await bcrypt.compare(value, hashe)
+    return isValid
+  }
+
+  async hash (value: string): Promise<string> {
+    const hash = await bcrypt.hash(value, this.salt)
+    return hash
+  }
+}
---
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken'
+import { JwtAdatper } from './jwt-adapter'
+
+jest.mock('jsonwebtoken', () => ({
+  async sign  (): Promise<string> {
+    return new Promise(resolve => resolve('any_token'))
+  }
+}))
+const makeSut = (): JwtAdatper => {
+  return new JwtAdatper('secret')
+}
+describe('jwt Adapter', () => {
+  test('Should call sign with correct values',async () => {
+    const sut = makeSut()
+    const signSpy = jest.spyOn(jwt, 'sign')
+    await sut.encrypt('any_value')
+    expect(signSpy).toHaveBeenCalledWith({ id: 'any_value' }, 'secret')
+  })
+  test('Should return a token on sign success',async () => {
+    const sut = makeSut()
+    const accessToken = await sut.encrypt('any_value')
+    expect(accessToken).toBe('any_token')
+  })
+  test('Should throws if sign throws',async () => {
+    const sut = makeSut()
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
+  })
+})
