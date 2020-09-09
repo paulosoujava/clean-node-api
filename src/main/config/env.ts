b0fc4a411a8dd7bca0d77e71aa3c06d68756bce8
PAULO OLIVEIRA <paulosoujava@gmail.com>
2020-09-09 09:57:09 -0300

    Update env.ts

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,5 +2,5 @@ export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://admin:admin@localhost:27017/',
   port: process.env.PORT || 5050,
   salt: 12,
-  jwtSecret: 'secret' || 'PauLo*9*&'
+  jwtSecret: 'secret' || 'abc'
 }
